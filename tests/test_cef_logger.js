eed158b6dcc19f3ac9486708a2016185873b0007
Jed Parsons <jedp@me.com>
2012-07-17 22:52:18 -0700

    Added logger output tests

---
+++ tests/test_cef_logger.js
@@ -0,0 +1,74 @@
+var vows = require('vows');
+var assert = require('assert');
+var cef = require('../lib/cef');
+var util = require('util');
+
+var logger = new cef.Logger(
+  {
+    vendor: 'Steinway',
+    product: 'Piano',
+    version: 'B'
+  }
+);
+
+// Mock console.log
+logger.logToConsole = function(message) {
+  return message;
+};
+
+// macros for testing logging functions
+function resultIs(expected) {
+  return function(err, result) {
+    assert.equal(expected, result);
+  };
+}
+
+function produces(string) {
+  var context = {
+    topic: function() {
+      var params = {
+        name: "Out of tune",
+        signature: "6/8"
+      };
+      return logger[this.context.name](params);
+    }
+  };
+
+  context["works"] = resultIs(string);
+  return context;
+}
+
+var suite = vows.describe("Logger")
+
+.addBatch({
+  "The logger": {
+    topic: function() {
+      var config = {
+        vendor: 'Steinway',
+        product: 'Piano',
+        version: 'B'
+      };
+      return new cef.Logger(config);
+    },
+
+    "has a formatter": function(logger) {
+      assert(typeof logger.formatter === 'object');
+    },
+
+    "emergency": produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|10"),
+    "alert":     produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|9"),
+    "critical":  produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|8"),
+    "error":     produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|7"),
+    "warning":   produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|6"),
+    "warn":      produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|6"),
+    "notice":    produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|5"),
+    "info":      produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|4"),
+    "debug":     produces("CEF:0|Steinway|Piano|B|6/8|Out of tune|3")
+  }
+});
+
+if (process.argv[1] === __filename) {
+  suite.run();
+} else {
+  suite.export(module);
+}
